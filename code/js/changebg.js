d2b0bebb9954afb0e21fefd79692fd4bd97e1c9a
noahmcge <corphos111@gmail.com>
2018-12-14 10:23:33 -0800

    aa

--- code/js/changebg.js
+++ code/js/changebg.js
@@ -1,5 +1,5 @@
 function backgr(){
-var randimg = Math.floor(Math.random()*4);
+var randimg = Math.floor(Math.random()*12);
 if (randimg==0){
 document.getElementById('bod').className="bg1";
 }else if (randimg==1){
@@ -8,5 +8,21 @@ document.getElementById('bod').className="bg2";
 document.getElementById('bod').className="bg3";
 }else if (randimg==3){
 document.getElementById('bod').className="bg4";
+}else if (randimg==4){
+document.getElementById('bod').className="bg5";
+}else if (randimg==5){
+document.getElementById('bod').className="bg6";
+}else if (randimg==6){
+document.getElementById('bod').className="bg7";
+}else if (randimg==7){
+document.getElementById('bod').className="bg8";
+}else if (randimg==8){
+document.getElementById('bod').className="bg9";
+}else if (randimg==9){
+document.getElementById('bod').className="bg10";
+}else if (randimg==10){
+document.getElementById('bod').className="bg11";
+}else if (randimg==11){
+document.getElementById('bod').className="bg12";
 }
 }
